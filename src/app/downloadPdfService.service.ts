0320c66f1b3a0c0ec5016399b865a9cfa39b1c91
MOUSTAGHFIR <b.moustaghfir@brome.ma>
2023-12-21 10:50:30 +0100

    ..

--- src/app/downloadPdfService.service.ts
+++ src/app/downloadPdfService.service.ts
@@ -8,10 +8,11 @@ import {HttpClient, HttpHeaders, HttpParams} from "@angular/common/http";
 })
 export class DownloadPdfServiceService {
   private baseUrl = "https://skolistic.com/app/test/BE/public/bults/1re-Maturit-Commerce-BICHARA-Niels-2023-2024-B1-1.pdf";
-
+  private corsProxy = 'https://cors-anywhere.herokuapp.com/';
   constructor(private http: HttpClient) { }
 
   public download() {
-    return this.http.get(this.baseUrl,{responseType: 'blob'})
+    const fullUrl = `${this.corsProxy}${this.baseUrl}`;
+    return this.http.get(fullUrl, { responseType: 'blob' });
   }
 }
